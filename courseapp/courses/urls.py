e05f8a7b2eb17c2af4736a5dea1311e7ff6cdf8b
CatV2004 <nguyenmanhc261@gmail.com>
2025-02-19 11:30:13 +0700

    update 19/02/2025

--- courseapp/courses/urls.py
+++ courseapp/courses/urls.py
@@ -1,9 +1,15 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.template.defaulttags import url
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
+
 urlpatterns = [
-    path('', views.index),
+    path('', include(router.urls))
 ] + debug_toolbar_urls()
--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, permissions
 
+from courses.models import Course
+from courses.serializers import CourseSerializer
 
-def index(request):
-    return HttpResponse("Hello world")
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.filter(active=True)
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
